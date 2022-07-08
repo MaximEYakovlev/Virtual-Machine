3cd9d1fc7ec0c30ec5d7cd1617348d25be5019ec
Maxim Yakovlev <it.trainee@yandex.ru>
2022-07-08 23:34:00 +0300

    abstract stack implementation

--- index.js
+++ index.js
@@ -21,11 +21,73 @@ const writableBytes = new Uint8Array(memory.buffer);
 
 const cpu = new CPU(memory);
 
+const subroutineAddress = 0x3000;
 let i = 0;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x33;
+writableBytes[i++] = 0x33;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x22;
+writableBytes[i++] = 0x22;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x11;
+writableBytes[i++] = 0x11;
+
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0x12;
+writableBytes[i++] = 0x34;
+writableBytes[i++] = R1;
+
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0x56;
+writableBytes[i++] = 0x78;
+writableBytes[i++] = R4;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x00;
+writableBytes[i++] = 0x00;
+
+writableBytes[i++] = instructions.CAL_LIT;
+writableBytes[i++] = (subroutineAddress & 0xff00) >> 8;
+writableBytes[i++] = subroutineAddress & 0x00ff;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x44;
+writableBytes[i++] = 0x44;
+
+// Subroutine ...
+i = subroutineAddress;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x01;
+writableBytes[i++] = 0x02;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x03;
+writableBytes[i++] = 0x04;
+
+writableBytes[i++] = instructions.PSH_LIT;
+writableBytes[i++] = 0x05;
+writableBytes[i++] = 0x06;
+
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0x07;
+writableBytes[i++] = 0x08;
+writableBytes[i++] = R1;
+
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0x09;
+writableBytes[i++] = 0x0a;
+writableBytes[i++] = R8;
+
+writableBytes[i++] = instructions.RET;
 
 cpu.debug();
 cpu.viewMemoryAt(cpu.getRegister("ip"));
-cpu.viewMemoryAt(0xffff - 1 - 6);
+cpu.viewMemoryAt(0xffff - 1 - 42, 44);
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -36,5 +98,5 @@ rl.on("line", () => {
   cpu.step();
   cpu.debug();
   cpu.viewMemoryAt(cpu.getRegister("ip"));
-  cpu.viewMemoryAt(0xffff - 1 - 6);
+  cpu.viewMemoryAt(0xffff - 1 - 42, 44);
 });
