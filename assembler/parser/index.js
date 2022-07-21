aa08a66c8967edcc4b6f140de03e0df7099465c7
Maxim Yakovlev <it.trainee@yandex.ru>
2022-07-21 23:16:30 +0300

    the parser is completed

--- assembler/parser/index.js
+++ assembler/parser/index.js
@@ -11,6 +11,7 @@ const deepLog = (x) =>
 
 const asType = (type) => (value) => ({ type, value });
 const mapJoin = (parser) => parser.map((items) => items.join(""));
+const peek = A.lookAhead(A.regex(/^./));
 
 const upperOrLowerStr = (s) =>
   A.choice([A.str(s.toUpperCase()), A.str(s.toLowerCase())]);
@@ -31,15 +32,66 @@ const register = A.choice([
 ]).map(asType("REGISTER"));
 
 const hexDigit = A.regex(/^[0-9A-Fa-f]/);
-const hexLiteral = A.char("s")
+const hexLiteral = A.char("$")
   .chain(() => mapJoin(A.many1(hexDigit)))
   .map(asType("HEX_LITERAL"));
+
+const validIdentifier = mapJoin(
+  A.sequenceOf([
+    A.regex(/^[a-zA-Z_]/),
+    A.possibly(A.regex(/^[a-zA-Z0-9_]+/)).map((x) => (x === null ? "" : x)),
+  ])
+);
+
+const variable = A.char("!")
+  .chain(() => validIdentifier)
+  .map(asType("VARIABLE"));
+
+const operator = A.choice([
+  A.char("+").map(asType("OP_PLUS")),
+  A.char("-").map(asType("OP_MINUS")),
+  A.char("*").map(asType("OP_MULTIPLY")),
+]);
+
+const squareBracketExpr = A.coroutine(function* () {
+  yield A.char("[");
+  yield A.optionalWhitespace;
+
+  const states = {
+    EXPECT_ELEMENT: 0,
+    EXPECT_OPERATOR: 1,
+  };
+
+  const expr = [];
+  let state = states.EXPECT_ELEMENT;
+
+  while (true) {
+    if (state === states.EXPECT_ELEMENT) {
+      const result = yield A.choice([bracketedExpr, hexLiteral, variable]);
+      expr.push(result);
+      state = states.EXPECT_OPERATOR;
+      yield A.optionalWhitespace;
+    } else if (state === states.EXPECT_OPERATOR) {
+      const nextChar = yield peek;
+      if (nextChar === "]") {
+        yield A.char("]");
+        yield A.optionalWhitespace;
+        break;
+      }
+      const result = yield operator;
+      expr.push(result);
+      state = states.EXPECT_ELEMENT;
+      yield A.optionalWhitespace;
+    }
+  }
+  return asType("SQUARE_BRACKET_EXPRESSION")(expr);
+});
 
 const movLitToReg = A.coroutine(function* () {
   yield upperOrLowerStr("mov");
   yield A.whitespace;
 
-  const arg1 = yield hexLiteral;
+  const arg1 = yield A.choice([hexLiteral, squareBracketExpr]);
 
   yield A.optionalWhitespace;
   yield A.char(",");
@@ -54,5 +106,5 @@ const movLitToReg = A.coroutine(function* () {
   });
 });
 
-const res = movLitToReg.run("mov s42, r4");
+const res = movLitToReg.run("mov $42, r4");
 deepLog(res);
