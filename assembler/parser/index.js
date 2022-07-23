188212e03ea09add176fb45cc2381af02d634511
Maxim Yakovlev <it.trainee@yandex.ru>
2022-07-23 23:26:03 +0300

    added: the order of operations according to priority

--- assembler/parser/index.js
+++ assembler/parser/index.js
@@ -16,6 +16,54 @@ const peek = A.lookAhead(A.regex(/^./));
 const upperOrLowerStr = (s) =>
   A.choice([A.str(s.toUpperCase()), A.str(s.toLowerCase())]);
 
+const binaryOperation = asType("BINARY_OPERATION");
+const disambiguateOrderOfOperations = (expr) => {
+  if (
+    expr.type !== "SQUARE_BRACKET_EXPRESSION" &&
+    expr.type !== "BRACKETED_EXPRESSION"
+  ) {
+    return expr;
+  }
+
+  if (expr.value.length === 1) {
+    return expr.value[0];
+  }
+
+  const priorities = {
+    OP_MULTIPLY: 2,
+    OP_PLUS: 1,
+    OP_MINUS: 0,
+  };
+
+  let candidateExpression = {
+    priority: -Infinity,
+  };
+
+  for (let i = 1; i < expr.value.length; i += 2) {
+    const level = priorities[expr.value[i].type];
+    if (level > candidateExpression.priority) {
+      candidateExpression = {
+        priority: level,
+        a: i - 1,
+        b: i + 1,
+        op: expr.value[i],
+      };
+    }
+  }
+
+  const newExpression = asType("BRACKETED_EXPRESSION")([
+    ...expr.value.slice(0, candidateExpression.a),
+    binaryOperation({
+      a: disambiguateOrderOfOperations(expr.value[candidateExpression.a]),
+      b: disambiguateOrderOfOperations(expr.value[candidateExpression.b]),
+      op: candidateExpression.op,
+    }),
+    ...expr.value.slice(candidateExpression.b + 1),
+  ]);
+
+  return disambiguateOrderOfOperations(newExpression);
+};
+
 const register = A.choice([
   upperOrLowerStr("r1"),
   upperOrLowerStr("r2"),
@@ -157,7 +205,7 @@ const squareBracketExpr = A.coroutine(function* () {
     }
   }
   return asType("SQUARE_BRACKET_EXPRESSION")(expr);
-});
+}).map(disambiguateOrderOfOperations);
 
 const movLitToReg = A.coroutine(function* () {
   yield upperOrLowerStr("mov");
