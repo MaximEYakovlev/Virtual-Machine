478ae6a1f778172f1bd249005f6da7af3a390717
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-01 19:45:10 +0300

    instructions directory: added file index.js

---
+++ instructions/index.js
@@ -0,0 +1,9 @@
+const { meta } = require("./meta");
+
+const indexBy = (array, prop) =>
+  array.reduce((output, item) => {
+    output[item[prop]] = item;
+    return output;
+  }, {});
+
+module.exports = indexBy(meta, "instruction");
