3ca1b8b5eea74b9d56a00b5439e92a090a7679ce
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-04 21:53:25 +0300

    program writing...

--- assembler/index.js
+++ assembler/index.js
@@ -16,3 +16,41 @@ const registerMap = {
   sp: 10,
   fp: 11,
 };
+
+const exampleProgram = [
+  "mov $4200, r1",
+  "mom r1, &0060",
+  "mov $1300, r1",
+  "mov &0060, r2",
+  "add r1, r2",
+].join("\n");
+
+const parsedOutput = parser.run(exampleProgram);
+
+const machineCode = [];
+
+const encodeLitOrMem = (lit) => {
+  const hexVal = parseInt(lit.value, 16);
+  const highByte = (hexVal & 0xff00) >> 8;
+  const lowByte = hexVal & 0x00ff;
+  machineCode.push(highByte, lowByte);
+};
+
+const encodeLit8 = (lit) => {
+  const hexVal = parseInt(lit.value, 16);
+  const lowByte = hexVal & 0x00ff;
+  machineCode.push(lowByte);
+};
+
+const encodeReg = (reg) => {
+  const mappedReg = registerMap[reg.value];
+  machineCode.push(mappedReg);
+};
+
+parsedOutput.result.forEach((instruction) => {
+  const metadata = instructions[instruction.value.instruction];
+  machineCode.push(metadata.opcode);
+
+
+
+});
