40415964c222ad23c2ca8ef671912d704bac22e0
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-08 22:39:38 +0300

    bank switching implementation

--- index.js
+++ index.js
@@ -1,55 +1,29 @@
-const readline = require("readline");
 const createMemory = require("./create-memory");
 const CPU = require("./cpu");
-const instructions = require("./instructions");
-const MemoryMapper = require("./memory-mapper");
-const createScreenDevice = require("./screen-device");
-
-const IP = 0;
-const ACC = 1;
-const R1 = 2;
-const R2 = 3;
-const R3 = 4;
-const R4 = 5;
-const R5 = 6;
-const R6 = 7;
-const R7 = 8;
-const R8 = 9;
-const SP = 10;
-const FP = 11;
+const MemoryMapper = require("./memory-mapper.js");
 
 const MM = new MemoryMapper();
 
-const memory = createMemory(256 * 256);
-MM.map(memory, 0, 0xffff);
+const dataViewMethods = ["getUint8", "getUint16", "setUint8", "setUint16"];
 
-// Map 0xFF bytes of the address space to an "output device" - just stdout
-MM.map(createScreenDevice(), 0x3000, 0x30ff, true);
+const createBankedMemory = (n, bankSize, cpu) => {
+  const bankBuffers = Array.from(
+    { length: n },
+    () => new ArrayBuffer(bankSize)
+  );
+  const banks = bankBuffers.map((ab) => new DataView(ab));
 
-const writableBytes = new Uint8Array(memory.buffer);
+  const forwardToDataView =
+    (name) =>
+    (...args) => {
+      const bankIndex = cpu.getRegister("mb") % n;
+      const memoryBankToUse = banks[bankIndex];
+      return memoryBankToUse[name](...args);
+    };
 
-const cpu = new CPU(MM);
-let i = 0;
-
-const writeCharToScreen = (char, command, position) => {
-  writableBytes[i++] = instructions.MOV_LIT_REG;
-  writableBytes[i++] = command;
-  writableBytes[i++] = char.charCodeAt(0);
-  writableBytes[i++] = R1;
-
-  writableBytes[i++] = instructions.MOV_REG_MEM;
-  writableBytes[i++] = R1;
-  writableBytes[i++] = 0x30;
-  writableBytes[i++] = position;
+  const interface = dataViewMethods.reduce((dvOut, fnName) => {
+    dvOut[fnName] = forwardToDataView(fnName);
+    return dvOut;
+  }, {});
+  return interface;
 };
-
-writeCharToScreen(" ", 0xff, 0);
-
-for (let index = 0; index <= 0xff; index++) {
-  const command = index % 2 === 0 ? 0x01 : 0x02;
-  writeCharToScreen("*", command, index);
-}
-
-writableBytes[i++] = instructions.HLT;
-
-cpu.run();
