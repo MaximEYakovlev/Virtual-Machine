85085f03900650ad94133d6b11b6017917dedf43
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-15 12:00:00 +0300

    assembler / parser / index.js: made changes

--- assembler/parser/index.js
+++ assembler/parser/index.js
@@ -1,5 +1,9 @@
-const instructionsParser = require("./instructions");
 const A = require("arcsecond");
+const constantParser = require("./constant");
+const { data8, data16 } = require("./data");
+const instructionsParser = require("./instructions");
 const { label } = require("./common");
 
-module.exports = A.many(A.choice([instructionsParser, label]));
+module.exports = A.many(
+  A.choice([constantParser, data8, data16, instructionsParser, label])
+).chain((res) => A.endOfInput.map(() => res));
